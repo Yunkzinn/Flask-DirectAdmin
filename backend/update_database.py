1048bb70c838a09829fc0f130e21904ab24af164
Leonardo-martins-techsolutio <leonardo.martins@techsolutio.com>
2024-08-23 20:02:15 -0300

    Ultimo update

--- backend/update_database.py
+++ backend/update_database.py
@@ -5,20 +5,26 @@ from datetime import datetime
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
-# Adicione a coluna created_at
-try:
-    cursor.execute('ALTER TABLE recommendation ADD COLUMN created_at TIMESTAMP')
-    print("Coluna 'created_at' adicionada com sucesso.")
-except sqlite3.OperationalError as e:
-    print(f"Erro: {e}")
+# Função para adicionar a coluna created_at e atualizar registros existentes
+def add_created_at_column(table_name):
+    try:
+        cursor.execute(f'ALTER TABLE {table_name} ADD COLUMN created_at TIMESTAMP')
+        print(f"Coluna 'created_at' adicionada com sucesso na tabela {table_name}.")
+    except sqlite3.OperationalError as e:
+        print(f"Erro ao adicionar a coluna na tabela {table_name}: {e}")
 
-# Atualize todos os registros existentes com a data atual
-cursor.execute('SELECT id FROM recommendation')
-recommendations = cursor.fetchall()
-now = datetime.utcnow()
+    # Atualize todos os registros existentes com a data atual
+    cursor.execute(f'SELECT id FROM {table_name}')
+    records = cursor.fetchall()
+    now = datetime.utcnow()
 
-for j in recommendations:
-    cursor.execute('UPDATE recommendation SET created_at = ? WHERE id = ?', (now, j[0]))
+    for record in records:
+        cursor.execute(f'UPDATE {table_name} SET created_at = ? WHERE id = ?', (now, record[0]))
+
+# Adicione a coluna created_at nas tabelas recommendation, jurisprudence e article
+add_created_at_column('recommendation')
+add_created_at_column('jurisprudence')
+add_created_at_column('article')
 
 # Salve as alterações e feche a conexão
 conn.commit()
